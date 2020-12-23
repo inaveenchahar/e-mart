5a401ac8684c5f8c478bfdbb0efe121b4b252d11
inaveenchahar <inaveenchahar@gmail.com>
2020-12-23 18:18:08 +0530

    feat(seller): dashboard created

--- seller/urls.py
+++ seller/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'seller'
 
 urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('order-status/', views.index_seller, name='index_seller'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from cart.models import Cart
 
 # Create your views here.
+
+
+def dashboard(request):
+    if request.user.is_superuser:
+        all_users = User.objects.filter(is_superuser=False).count()
+        all_orders = Cart.objects.filter(completed=True).count()
+        ordered_items = Cart.objects.filter(completed=True, delivered=False)
+        delivered_items = Cart.objects.filter(completed=True, delivered=True)
+        return render(request, 'dashboard.html', {'all_users': all_users, 'all_orders': all_orders, 'ordered_items': ordered_items, 'delivered_items': delivered_items})
+    else:
+        return redirect('main:homepage')
 
 
 def index_seller(request):
