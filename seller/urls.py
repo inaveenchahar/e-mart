eb9e709678ea4b1713aa880c11dc0b503edd3f55
inaveenchahar <inaveenchahar@gmail.com>
2020-12-25 09:31:56 +0530

    feat(seller): shipped orders view created and clickable on dashboard

--- seller/urls.py
+++ seller/urls.py
@@ -6,5 +6,6 @@ app_name = 'seller'
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('yet-to-be-shipped-orders/', views.yet_shipped_orders, name='yet_shipped'),
+    path('shipped-orders/', views.shipped_orders, name='shipped_orders'),
     path('order-status/', views.index_seller, name='index_seller'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -8,8 +8,8 @@ from cart.models import Cart
 def dashboard(request):
     if request.user.is_superuser:
         all_orders = Cart.objects.filter(completed=True).count()
-        yet_shipped_orders = Cart.objects.filter(shipped=False, delivered=False).count()
-        ordered_items = Cart.objects.filter(completed=True, delivered=False)
+        yet_shipped_orders = Cart.objects.filter(shipped=False, delivered=False, completed=True).count()
+        ordered_items = Cart.objects.filter(completed=True, shipped=False, delivered=False)
         delivered_items = Cart.objects.filter(completed=True, delivered=True)
         return render(request, 'dashboard.html', {'all_orders': all_orders,
                                                   'ordered_items': ordered_items,
@@ -31,8 +31,18 @@ def index_seller(request):
 
 def yet_shipped_orders(request):
     if request.user.is_superuser:
-        all_orders = Cart.objects.filter(shipped=False, delivered=False)
+        all_orders = Cart.objects.filter(completed=True, shipped=False, delivered=False)
         return render(request, 'yet_shipped_orders.html', {'all_orders': all_orders})
     else:
         return redirect('main:homepage')
+
+
+def shipped_orders(request):
+    if request.user.is_superuser:
+        all_orders = Cart.objects.filter(completed=True, shipped=True, delivered=False)
+        return render(request, 'shipped_orders.html', {'all_orders': all_orders})
+    else:
+        return redirect('main:homepage')
+
+
 
